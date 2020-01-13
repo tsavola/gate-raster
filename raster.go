efb1db33b20c19cfb0ced92786b32e945d1c4f62
Timo Savola <timo.savola@iki.fi>
2020-01-13 20:59:38 +0200

    single-threaded SDL calling, multi-window support

--- raster.go
+++ raster.go
@@ -10,6 +10,7 @@ import (
 	"encoding/binary"
 	"log"
 	"runtime"
+	"sync"
 
 	"github.com/tsavola/gate/packet"
 	"github.com/tsavola/gate/service"
@@ -22,8 +23,6 @@ const (
 )
 
 const initialScale = 4
-
-var inited bool
 
 func InitServices(registry *service.Registry) (err error) {
 	registry.Register(raster{})
@@ -77,45 +76,42 @@ func (inst *instance) restore(snapshot []byte) (err error) {
 
 func (inst *instance) Resume(ctx context.Context, send chan<- packet.Buf) {
 	if inst.inCall {
+		initRun()
 		inst.handleReply(ctx, send)
 	}
 }
 
 func (inst *instance) Handle(ctx context.Context, send chan<- packet.Buf, p packet.Buf) {
 	if p.Domain() == packet.DomainCall {
+		initRun()
 		inst.inCall = true
 		inst.handleCall(ctx, send, p)
 	}
 }
 
 func (inst *instance) handleCall(ctx context.Context, send chan<- packet.Buf, p packet.Buf) {
-	if !inited {
-		runtime.LockOSThread()
-
-		if err := sdl.Init(sdl.INIT_VIDEO | sdl.INIT_EVENTS); err != nil {
-			panic(err)
-		}
-
-		inited = true
-	}
-
 	if inst.surface == nil {
-		s, err := sdl.CreateRGBSurfaceWithFormat(0, 320, 200, 32, sdl.PIXELFORMAT_ARGB8888)
-		if err == nil {
-			inst.surface = s
-		} else {
-			log.Printf("%s: %v", ServiceName, err)
-		}
+		do(func() {
+			s, err := sdl.CreateRGBSurfaceWithFormat(0, 320, 200, 32, sdl.PIXELFORMAT_ARGB8888)
+			if err == nil {
+				inst.surface = s
+			} else {
+				log.Printf("%s: %v", ServiceName, err)
+			}
+		})
 	}
 
 	if inst.window == nil {
-		w, err := sdl.CreateWindow(ServiceName, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
-			320*initialScale, 200*initialScale, sdl.WINDOW_SHOWN|sdl.WINDOW_RESIZABLE)
-		if err == nil {
-			inst.window = w
-		} else {
-			log.Printf("%s: %v", ServiceName, err)
-		}
+		do(func() {
+			w, err := sdl.CreateWindow(ServiceName, sdl.WINDOWPOS_UNDEFINED, sdl.WINDOWPOS_UNDEFINED,
+				320*initialScale, 200*initialScale, sdl.WINDOW_SHOWN|sdl.WINDOW_RESIZABLE)
+			if err == nil {
+				subscribeWindowEvents(w)
+				inst.window = w
+			} else {
+				log.Printf("%s: %v", ServiceName, err)
+			}
+		})
 	}
 
 	inst.draw(p)
@@ -126,60 +122,67 @@ func (inst *instance) handleReply(ctx context.Context, send chan<- packet.Buf) {
 	reply := bytes.NewBuffer(packet.MakeCall(inst.Code, 8)[:packet.HeaderSize])
 
 	if inst.window != nil {
-		for {
-			event := sdl.PollEvent()
-			if event == nil {
-				break
-			}
+		do(func() {
+			for {
+				event := pollWindowEvent(inst.window)
+				if event == nil {
+					break
+				}
 
-			b := make([]byte, 8)
+				b := make([]byte, 8)
 
-			switch e := event.(type) {
-			case *sdl.QuitEvent:
-				b[0] = 1
+				switch e := event.(type) {
+				case *sdl.WindowEvent:
+					if e.Event == sdl.WINDOWEVENT_CLOSE {
+						b[0] = 1
+					}
 
-			case *sdl.KeyboardEvent:
-				if e.Type == sdl.KEYDOWN {
-					if e.Keysym.Scancode == sdl.SCANCODE_RALT {
-						if inst.grabbed {
-							inst.window.SetGrab(false)
-							sdl.SetRelativeMouseMode(false)
-							sdl.ShowCursor(sdl.ENABLE)
-							inst.grabbed = false
+				case *sdl.KeyboardEvent:
+					if e.Type == sdl.KEYDOWN {
+						if e.Keysym.Scancode == sdl.SCANCODE_RALT {
+							if inst.grabbed {
+								inst.window.SetGrab(false)
+								sdl.SetRelativeMouseMode(false)
+								sdl.ShowCursor(sdl.ENABLE)
+								inst.grabbed = false
+							}
+						} else {
+							b[0] = 2
 						}
 					} else {
-						b[0] = 2
+						b[0] = 3
 					}
-				} else {
-					b[0] = 3
-				}
-				b[1] = byte(e.Keysym.Scancode)
+					b[1] = byte(e.Keysym.Scancode)
 
-			case *sdl.MouseButtonEvent:
-				if e.State == sdl.PRESSED {
-					if !inst.grabbed {
-						inst.window.SetGrab(true)
-						sdl.SetRelativeMouseMode(true)
-						sdl.ShowCursor(sdl.DISABLE)
-						inst.grabbed = true
-						continue
+				case *sdl.MouseButtonEvent:
+					if e.State == sdl.PRESSED {
+						if !inst.grabbed {
+							inst.window.SetGrab(true)
+							sdl.SetRelativeMouseMode(true)
+							sdl.ShowCursor(sdl.DISABLE)
+							inst.grabbed = true
+							continue
+						}
+						b[0] = 4
+					} else {
+						b[0] = 5
 					}
-					b[0] = 4
-				} else {
-					b[0] = 5
-				}
-				b[1] = e.Button
+					b[1] = e.Button
 
-			case *sdl.MouseMotionEvent:
-				if inst.grabbed {
-					b[0] = 6
-					binary.LittleEndian.PutUint16(b[4:], uint16(e.XRel))
-					binary.LittleEndian.PutUint16(b[6:], uint16(e.YRel))
+				case *sdl.MouseMotionEvent:
+					if inst.grabbed {
+						b[0] = 6
+						binary.LittleEndian.PutUint16(b[4:], uint16(e.XRel))
+						binary.LittleEndian.PutUint16(b[6:], uint16(e.YRel))
+					}
+
+				default:
+					continue
 				}
+
+				reply.Write(b)
 			}
-
-			reply.Write(b)
-		}
+		})
 	}
 
 	select {
@@ -196,44 +199,52 @@ func (inst *instance) draw(p packet.Buf) {
 		return
 	}
 
-	err := func() (err error) {
-		if inst.surface.MustLock() {
-			err = inst.surface.Lock()
-			if err != nil {
-				return
+	do(func() {
+		err := func() error {
+			if inst.surface.MustLock() {
+				if err := inst.surface.Lock(); err != nil {
+					return err
+				}
+				defer inst.surface.Unlock()
 			}
-			defer inst.surface.Unlock()
+
+			palette := p.Content()[:3*256]
+			pixels := p.Content()[3*256:]
+			dest := inst.surface.Pixels()
+
+			for y := int32(0); y < 200; y++ {
+				for x := int32(0); x < 320; x++ {
+					i := int(pixels[y*320+x])
+					dest[y*inst.surface.Pitch+x*4+0] = palette[3*i+2]
+					dest[y*inst.surface.Pitch+x*4+1] = palette[3*i+1]
+					dest[y*inst.surface.Pitch+x*4+2] = palette[3*i+0]
+					dest[y*inst.surface.Pitch+x*4+3] = 255
+				}
+			}
+
+			return nil
+		}()
+		if err != nil {
+			log.Printf("%s: %v", ServiceName, err)
+			return
 		}
 
-		palette := p.Content()[:3*256]
-		pixels := p.Content()[3*256:]
-		dest := inst.surface.Pixels()
-
-		for y := int32(0); y < 200; y++ {
-			for x := int32(0); x < 320; x++ {
-				i := int(pixels[y*320+x])
-				dest[y*inst.surface.Pitch+x*4+0] = palette[3*i+2]
-				dest[y*inst.surface.Pitch+x*4+1] = palette[3*i+1]
-				dest[y*inst.surface.Pitch+x*4+2] = palette[3*i+0]
-				dest[y*inst.surface.Pitch+x*4+3] = 255
-			}
+		s, err := inst.window.GetSurface()
+		if err != nil {
+			log.Printf("%s: %v", ServiceName, err)
+			return
 		}
 
-		return
-	}()
-	if err == nil {
-		if s, err := inst.window.GetSurface(); err == nil {
-			err = inst.surface.BlitScaled(nil, s, nil)
-			if err == nil {
-				err = inst.window.UpdateSurface()
-			}
+		if err := inst.surface.BlitScaled(nil, s, nil); err != nil {
+			log.Printf("%s: %v", ServiceName, err)
+			return
 		}
-	}
 
-	if err != nil {
-		log.Printf("%s: %v", ServiceName, err)
-		return
-	}
+		if err := inst.window.UpdateSurface(); err != nil {
+			log.Printf("%s: %v", ServiceName, err)
+			return
+		}
+	})
 }
 
 func (inst *instance) Suspend() (snapshot []byte) {
@@ -249,12 +260,132 @@ func (inst *instance) Suspend() (snapshot []byte) {
 
 func (inst *instance) Shutdown() {
 	if inst.window != nil {
-		inst.window.Destroy()
+		do(func() {
+			unsubscribeWindowEvents(inst.window)
+			inst.window.Destroy()
+			inst.window = nil
+		})
 	}
 
 	if inst.surface != nil {
-		inst.surface.Free()
+		do(func() {
+			inst.surface.Free()
+			inst.surface = nil
+		})
 	}
 }
+
+type task struct {
+	f func()
+	c chan struct{}
+}
+
+func do(f func()) {
+	c := make(chan struct{})
+	tasks <- task{f, c}
+	<-c
+}
+
+func initRun() {
+	runInit.Do(func() {
+		go runTasks()
+	})
+}
+
+func runTasks() {
+	runtime.LockOSThread()
+	defer runtime.UnlockOSThread()
+
+	if err := sdl.Init(sdl.INIT_VIDEO | sdl.INIT_EVENTS); err != nil {
+		panic(err)
+	}
+	defer sdl.Quit()
+
+	for x := range tasks {
+		for {
+			event := sdl.PollEvent()
+			if event == nil {
+				break
+			}
+
+			var id uint32
+
+			switch e := event.(type) {
+			case *sdl.WindowEvent:
+				id = e.WindowID
+				clone := *e
+				event = &clone
+
+			case *sdl.KeyboardEvent:
+				id = e.WindowID
+				clone := *e
+				event = &clone
+
+			case *sdl.MouseButtonEvent:
+				id = e.WindowID
+				clone := *e
+				event = &clone
+
+			case *sdl.MouseMotionEvent:
+				id = e.WindowID
+				clone := *e
+				event = &clone
+
+			default:
+				event = nil
+			}
+
+			if event != nil {
+				if queue, ok := windowEvents[id]; ok {
+					windowEvents[id] = append(queue, event)
+				}
+			}
+		}
+
+		x.f()
+		close(x.c)
+	}
+}
+
+func subscribeWindowEvents(w *sdl.Window) {
+	id, err := w.GetID()
+	if err != nil {
+		panic(err)
+	}
+
+	windowEvents[id] = nil
+}
+
+func unsubscribeWindowEvents(w *sdl.Window) {
+	id, err := w.GetID()
+	if err != nil {
+		panic(err)
+	}
+
+	delete(windowEvents, id)
+}
+
+func pollWindowEvent(w *sdl.Window) (e sdl.Event) {
+	id, err := w.GetID()
+	if err != nil {
+		panic(err)
+	}
+
+	queue, ok := windowEvents[id]
+	if !ok {
+		panic(w)
+	}
+	if len(queue) == 0 {
+		return nil
+	}
+
+	e = queue[0]
+	windowEvents[id] = queue[1:]
+	return
+}
+
+var runInit sync.Once
+var tasks = make(chan task)
+var windowEvents = make(map[uint32][]sdl.Event)
 
 func main() {}
